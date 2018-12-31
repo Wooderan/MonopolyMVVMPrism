b82de24fef659b2c36ad0d11f34edb61cd4d652d
Eugen Krivulia <eugentopolevski@gmail.com>
2018-12-31 11:13:33 +0300

    Добавьте файлы проекта.

---
+++ Monopoly/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Monopoly.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
